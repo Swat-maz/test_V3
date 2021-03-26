98309f6559a6ea856a5249a02b611498adbe5f84
Swat-maz <igor.mazur.selets@gmail.com>
2021-03-26 16:35:24 +0200

    gotova forma ale validatsiu zrobutu po inshomy

--- js/script.js
+++ js/script.js
@@ -5,21 +5,32 @@ $(document).ready(function() { // Ждём загрузки страницы
 		draggable:false,
 		autoplay:true,
 		autoplaySpeed:4000,
+		waitForAnimate:false,
 		infinite:true,
-		sopead:1000,
+		speed:1000,
 		fade:true,
 		pauseOnFocus:false,
-		appendArrows:$('.buttons')
+		appendArrows:$('.buttons'),
 		// centerMode:true
 	});
 
 
 	$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
-		$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
+		$(".popup").fadeOut(500);	// Медленно убираем всплывающее окно
 	});
 
 });
 
 function showPopup() {
-	$(".popup").fadeIn(800); // Медленно выводим изображение
+	$(".popup").fadeIn(500); // Медленно выводим изображение
+};
+
+function clearPopup() {
+	$(".popup").fadeOut(500);
+	$('#myform')[0].reset();
+	$("input").attr("placeholder", "");
+};
+
+function validateForm() {
+	$("input").attr("placeholder", "Поле обязательно для заполнения");
 }
